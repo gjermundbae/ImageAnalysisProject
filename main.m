9e7c63fb7d961c9504e7f32a04227c88582261b9
gjermundbae <gjermundbae@hotmail.com>
2023-01-17 14:48:59 +0100

    inkludert film m/ lesing av film

--- main.m
+++ main.m
@@ -1,1 +1,13 @@
 a = 1;
+v = VideoReader('squash_vid_10sek.mp4');
+v.CurrentTime = 2.5;
+
+currAxes = axes;
+while hasFrame(v)
+    vidFrame = readFrame(v);
+    image(vidFrame, 'Parent', currAxes);
+    currAxes.Visible = 'off';
+    pause(1/v.FrameRate);
+end
+
+whos frame
